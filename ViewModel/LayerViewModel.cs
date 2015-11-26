9d95a664286f9d69a2b2f3520c3225560f921dc4
L00tefisk <Eivind.93@hotmail.com>
2015-11-26 21:55:40 +0100

    Oops.

---
+++ ViewModel/LayerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Controls;
+
+namespace LevelEditor.ViewModel
+{
+    public class Layer
+    {
+        public string Name { get; set; }
+
+        public Layer(string name)
+        {
+            Name = name;
+        }
+    }
+    public class LayerViewModel : ListView
+    {
+        public ObservableCollection<Layer> Layers { get; set; }
+
+        private Layer _selectedItem;
+        private LayerViewModel()
+        {
+            Layers = new ObservableCollection<Layer>();
+
+            for (int i = 0; i < 5; i++)
+                Layers.Add(new Layer("Layer "+i+1));
+
+            ItemsSource = Layers;
+
+            this.SelectedItem = Layers[0];
+        }
+    }
+}
